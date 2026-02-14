ba4c341db4c8fc3c1733492263972783bc84ea6a
RyRy79261 <ryanjnoble@gmail.com>
2026-02-14 12:18:12 +0100

    feat: enable auto-activation of new service workers by setting skipWaiting to true

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  skipWaiting: false,  // Allow manual control via SKIP_WAITING message
+  skipWaiting: true,  // Auto-activate new service workers immediately
   customWorkerDir: 'worker',  // Custom service worker extensions
   disable: process.env.NODE_ENV === 'development'
 });
